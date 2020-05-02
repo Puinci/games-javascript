bd1eb71099fe606c02ac590ea3521b0673d01c62
puinci <fabygandarillas182@gmail.com>
2020-05-02 15:01:33 -0400

    final result

--- connect-four/app.js
+++ connect-four/app.js
@@ -44,3 +44,35 @@ document.addEventListener('DOMContentLoaded', () => {
         [22, 23, 24, 25], [26, 25, 24, 23], [29, 30, 31, 32], [33, 32, 31, 30], [36, 37, 38, 39], [40, 39, 38, 37], [7, 14, 21, 28],
         [8, 15, 22, 29], [9, 16, 23, 30], [10, 17, 24, 31], [11, 18, 25, 32], [12, 19, 26, 33], [13, 20, 27, 34]
         ];
+
+         //now take the 4 values in earch winningArray & plug them into the squares values 
+         for(let y = 0; y < winningArrays.length; y++) {
+            const square1 = squares[winningArrays[y][0]];
+            const square2 = squares[winningArrays[y][1]];
+            const square3 = squares[winningArrays[y][2]];
+            const square4 = squares[winningArrays[y][3]];
+
+            //now check those arrays to see if they all have the class of player-one
+            if(square1.classList.contains('player-one') &&
+                square2.classList.contains('player-one') &&
+                square3.classList.contains('player-one') &&
+                square4.classList.contains('player-one')) {
+                    //if they do, player-one is passed as the winner
+                    result.innerHTML = 'Player one wins!'
+                    //remove ability to change result
+            } 
+            //now check to see if they all have the classname player two
+            else if (square1.classList.contains('player-two') &&
+                square2.classList.contains('player-two') &&
+                square3.classList.contains('player-two') &&
+                square4.classList.contains('player-two')) {
+                    //if they do, player-two is passed as the winner as well as the chip positions
+                    result.innerHTML = 'Player two wins!'
+            }
+        }
+    }
+
+    //add an event listener to each square that will trigger the checkBoard function on click
+    squares.forEach(square => square.addEventListener('click', checkBoard))
+
+})
