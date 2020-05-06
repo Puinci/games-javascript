4de33abca8379a6eadffc18815e7a42649f7f8e2
puinci <fabygandarillas182@gmail.com>
2020-05-05 21:30:40 -0400

    final result

--- space invaders/app.js
+++ space invaders/app.js
@@ -74,6 +74,52 @@ document.addEventListener('DOMContentLoaded', ()=>{
         clearInterval(invaderId)
       }
     }
-}
+    
+     //ADD LATER
+     if(alienInvadersTakenDown.length === alienInvaders.length) {
+        console.log(alienInvadersTakenDown.length)
+        console.log(alienInvaders.length)
+        resultDisplay.textContent = 'You Win'
+        clearInterval(invaderId)
+      }
+    }
+    invaderId = setInterval(moveInvaders, 500)
+  
+     //shoot at aliens
+  function shoot(e) {
+    let laserId
+    let currentLaserIndex = currentShooterIndex
+    //move the laser from the shooter to the alien invader
+    function moveLaser() {
+      squares[currentLaserIndex].classList.remove('laser')
+      currentLaserIndex -= width
+      squares[currentLaserIndex].classList.add('laser')
+      if(squares[currentLaserIndex].classList.contains('invader')) {
+        squares[currentLaserIndex].classList.remove('laser')
+        squares[currentLaserIndex].classList.remove('invader')
+        squares[currentLaserIndex].classList.add('boom')
 
+        setTimeout(() => squares[currentLaserIndex].classList.remove('boom'), 250)
+        clearInterval(laserId)
+
+        const alienTakenDown = alienInvaders.indexOf(currentLaserIndex)
+        alienInvadersTakenDown.push(alienTakenDown)
+        result++
+        resultDisplay.textContent = result
+      }
+
+      if(currentLaserIndex < width) {
+        clearInterval(laserId)
+        setTimeout(() => squares[currentLaserIndex].classList.remove('laser'), 100)
+      }
+    }
+
+    switch(e.keyCode) {
+      case 32:
+        laserId = setInterval(moveLaser, 100)
+        break
+    }
+  }
+
+  document.addEventListener('keyup', shoot)
 })
