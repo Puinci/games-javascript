493df23234ef13ec04f889efab3754f09296901a
puinci <fabygandarillas182@gmail.com>
2020-05-07 21:33:43 -0400

    move thr frog function

---
+++ frogger-level2/app.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const squares = document.querySelectorAll('.grid div')
+  const logsLeft = document.querySelectorAll('.log-left')
+  const logsRight = document.querySelectorAll('.log-right')
+  const carsLeft = document.querySelectorAll('.car-left')
+  const carsRight = document.querySelectorAll('.car-right')
+  const timeLeft = document.querySelector('#time-left')
+  const result = document.querySelector('#result')
+  const startBtn = document.querySelector('#button')
+  const width = 9
+  let currentIndex = 76
+  let currentTime = 20
+  let timerId
+
+  //move the Frog
+  function moveFrog(e) {
+    squares[currentIndex].classList.remove('frog')
+    switch(e.keyCode) {
+      case 37:
+        if(currentIndex % width !== 0) currentIndex -= 1
+        break
+      case 38:
+        if(currentIndex - width >= 0) currentIndex -= width
+        break
+      case 39:
+        if(currentIndex % width < width - 1) currentIndex += 1
+        break
+      case 40:
+        if (currentIndex + width < width * width) currentIndex += width
+        break
+    }
+    squares[currentIndex].classList.add('frog')
+    lose()
+    win()
+  }
+  
+})
